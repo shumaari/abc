d632b69c25fd8c9824ecf61fbbdf8a1b306b9426
tushar <imtushargupta@gmail.com>
2025-01-10 22:22:46 +0530

    Added new function to check if number is multiple of 3

--- src/abc.py
+++ src/abc.py
@@ -3,10 +3,10 @@ def main():
 
     num = float(input("Enter a number: "))
     print("Entered number is", is_num_odd(num))
+    print("Is the number a multiple of 3?", is_multiple_of_three(num))
 
 
 def is_num_odd(x):
-    
     if x <= 0 or int(x) != x:
         return "neither odd nor even"
     elif (x % 2):
@@ -15,6 +15,12 @@ def is_num_odd(x):
         return "even"
 
 
+def is_multiple_of_three(x):
+    if x % 3 == 0:
+        return "yes"
+    else:
+        return "no"
+
+
 if __name__ == "__main__":
     main()
-

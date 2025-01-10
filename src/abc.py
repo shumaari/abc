9ca420ab710f5fa0a819bcd079444df3cb8d252c
shumaarigg <96530719+shumaari@users.noreply.github.com>
2025-01-10 21:23:36 +0530

    Added new function "is_num_odd" and tests for it

--- src/abc.py
+++ src/abc.py
@@ -1,1 +1,20 @@
-print("hello abc")
+def main():
+    # print("hello abc")
+
+    num = float(input("Enter a number: "))
+    print("Entered number is", is_num_odd(num))
+
+
+def is_num_odd(x):
+    
+    if x <= 0 or int(x) != x:
+        return "neither odd nor even"
+    elif (x % 2):
+        return "odd"
+    else:
+        return "even"
+
+
+if __name__ == "__main__":
+    main()
+
